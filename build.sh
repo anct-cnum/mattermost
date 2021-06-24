f640b6d06323a40334f0bee0500303173fbe650d
Loic GUILLOIS <contact@loicguillois.fr>
2021-06-24 16:08:21 +0200

    configuration Mattermost

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/5.15.0/mattermost-5.15.0-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/5.36.1/mattermost-5.36.1-linux-amd64.tar.gz -O - | tar -xvz
+cp config.json mattermost/config
