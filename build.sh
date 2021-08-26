32a04a8eeed7bf3c125875c5d959ec89474b13ba
Sébastien Quirrion <sebastien@fitlab.fr>
2021-08-26 11:06:20 +0200

    Mise a jour Mattermost

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/5.36.1/mattermost-5.36.1-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/5.38.1/mattermost-5.38.1-linux-amd64.tar.gz -O - | tar -xvz
 cp config.json mattermost/config
