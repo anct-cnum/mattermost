7658aa9f413274e45f2c234e9459844493057d87
Sébastien Quirrion <sebastien@fitlab.fr>
2021-09-21 14:16:12 +0200

    Modification config restrictTeamInvite

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/5.38.1/mattermost-5.38.1-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/5.39.0/mattermost-5.39.0-linux-amd64.tar.gz -O - | tar -xvz
 cp config.json mattermost/config
