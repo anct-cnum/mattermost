586320260952afbba35ed97040741391992ac152
Sébastien Quirrion <sebastien@fitlab.fr>
2023-06-21 15:29:47 +0200

    Upgrade v7.10.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/7.10.2/mattermost-7.10.2-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/7.10.3/mattermost-7.10.3-linux-amd64.tar.gz -O - | tar -xvz
 mkdir ./mattermost/plugins/
 wget https://github.com/streamer45/mattermost-plugin-voice/releases/download/v0.3.0/com.mattermost.voice-0.3.0.tar.gz -O - | tar -xzvf - -C ./mattermost/plugins/
 cp config.json mattermost/config
