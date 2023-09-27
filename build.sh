08b6ce57336a3afef404f0694fd035b3c84e9334
Jean-Yves Stervinou <jy@stervinou.com>
2023-09-27 21:00:20 +0200

    Upgrade v 9.0.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/8.0.1/mattermost-8.0.1-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/9.0.0/mattermost-9.0.0-linux-amd64.tar.gz -O - | tar -xvz
 mkdir ./mattermost/plugins/
 wget https://github.com/streamer45/mattermost-plugin-voice/releases/download/v0.3.0/com.mattermost.voice-0.3.0.tar.gz -O - | tar -xzvf - -C ./mattermost/plugins/
 cp config.json mattermost/config
