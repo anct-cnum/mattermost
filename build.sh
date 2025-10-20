11b6b6f086277773dc126fefa8d3bbde3227fa76
ornella452 <ornella93@outlook.fr>
2025-10-20 15:57:47 +0200

    deploy version 10.12.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-wget https://releases.mattermost.com/9.11.2/mattermost-9.11.2-linux-amd64.tar.gz -O - | tar -xvz
+wget https://releases.mattermost.com/10.12.1/mattermost-10.12.1-linux-amd64.tar.gz -O - | tar -xvz
 mkdir ./mattermost/plugins/
 wget https://github.com/streamer45/mattermost-plugin-voice/releases/download/v0.3.0/com.mattermost.voice-0.3.0.tar.gz -O - | tar -xzvf - -C ./mattermost/plugins/
 cp config.json mattermost/config
